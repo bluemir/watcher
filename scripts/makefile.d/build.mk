f1eb25654288dde5326b665adb7c98942aa4818e
BlueMir <bluemir@bluemir.me>
2026-04-23 11:42:07 +0900

    Add make install command

--- scripts/makefile.d/build.mk
+++ scripts/makefile.d/build.mk
@@ -26,6 +26,27 @@ build/$(APP_NAME): $(GO_SOURCES) $(MAKEFILE_LIST) fmt vet
 		$(OPTIONAL_BUILD_ARGS) \
 		-o $@ .
 
+build/$(APP_NAME)-$(VERSION): $(GO_SOURCES) $(MAKEFILE_LIST) fmt vet
+	@$(MAKE) build/tools/go
+	@mkdir -p build
+	go build -v \
+		-trimpath \
+		-ldflags "\
+			-X '$(IMPORT_PATH)/internal/buildinfo.AppName=$(APP_NAME)' \
+			-X '$(IMPORT_PATH)/internal/buildinfo.Version=$(VERSION)' \
+			-X '$(IMPORT_PATH)/internal/buildinfo.BuildTime=$(shell date --rfc-3339=ns)' \
+		" \
+		$(OPTIONAL_BUILD_ARGS) \
+		-o $@ .
+
+INSTALL_DIR?=$(HOME)/.local/bin
+
+.PHONY: install
+install: build/$(APP_NAME)-$(VERSION) ## Install binary to $(INSTALL_DIR)
+	@mkdir -p $(INSTALL_DIR)
+	cp $< $(INSTALL_DIR)/$(APP_NAME)-$(VERSION)
+	ln -sf $(APP_NAME)-$(VERSION) $(INSTALL_DIR)/$(APP_NAME)
+
 build-tools: build/tools/go
 build/tools/go:
 	@which $(notdir $@) || echo "see https://golang.org/doc/install"
